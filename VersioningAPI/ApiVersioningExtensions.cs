07aa8f346f19526757b60349aeefc96c8a3b0e25
Corbyn Altman <corbyn.altman01@gmail.com>
2022-11-15 19:39:49 -0500

    first commit

---
+++ VersioningAPI/ApiVersioningExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VersioningAPI;
+
+internal static class ApiVersioningExtensions
+{
+    internal static IServiceCollection AddApiVersioningOptions(this IServiceCollection services)
+    {
+        return services.AddApiVersioning(opts =>
+        {
+            opts.AssumeDefaultVersionWhenUnspecified = true;
+            opts.DefaultApiVersion = new ApiVersion(1, 0);
+            opts.ReportApiVersions = true;
+        });
+    }
+
+    internal static IServiceCollection AddApiExplorerOptions(this IServiceCollection services)
+    {
+        return services.AddVersionedApiExplorer(opts =>
+        {
+            opts.GroupNameFormat = "'v'VVV";
+            opts.SubstituteApiVersionInUrl = true;
+        });
+    }
+}
---
+++ VersioningAPI/Program.cs
@@ -0,0 +1,29 @@
+using VersioningAPI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioningOptions().AddApiExplorerOptions();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
